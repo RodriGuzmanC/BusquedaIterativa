a92e959daf3a15aef88e52fa547ce82b8848ad74
Rodrigo Guzman <101233941+SoyRodri@users.noreply.github.com>
2023-09-12 15:30:57 -0500

    Agregar archivos de proyecto.

---
+++ ejercicio06/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace ejercicio06
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(92, 38);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(302, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyUp);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(92, 74);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(302, 150);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(519, 257);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Busqueda Iterativa";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ ejercicio06/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ejercicio06
+{
+    public partial class Form1 : Form
+    {
+        DataSet datosTemporales = new DataSet();
+        DataView contenido;
+        public void metodoFiltrar(string consultaSql, ref DataSet resultadoTemporal, string tabla)
+        {
+            try
+            {
+                string miRuta = "Data Source=DESKTOP-2E54QI4;Initial Catalog=miBase16;Integrated Security=True";
+                SqlConnection miConexion = new SqlConnection(miRuta);
+                SqlCommand nuevaInstancia = new SqlCommand(consultaSql, miConexion);
+                miConexion.Open();
+                SqlDataAdapter datos = new SqlDataAdapter(nuevaInstancia);
+                datos.Fill(resultadoTemporal, tabla);
+                datos.Dispose(); //libera los recursos utilizados
+                miConexion.Close();
+            }
+            catch(Exception mensajeSistema)
+            {
+                MessageBox.Show(mensajeSistema.Message);
+            }
+        }
+        //Inicializador
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //botones
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+            string datosEncontrados = "";
+            string[] textoBusqueda = this.textBox1.Text.Split(' ');
+            foreach (string palabra in textoBusqueda)
+            {
+                if (datosEncontrados.Length == 0)
+                {
+                    //Verificar si la palabra es un número
+                    if (int.TryParse(palabra, out int codigoNumero))
+                    {
+                        //Si es un número, aplicar la condición de igualdad en lugar de LIKE
+                        datosEncontrados = "(codigo = " + codigoNumero + " OR nombre LIKE '%" + palabra + "%' OR apellido LIKE '%" + palabra + "%')";
+                    }
+                    else
+                    {
+                        //Si no es un número, usar LIKE
+                        datosEncontrados = "(nombre LIKE '%" + palabra + "%' OR apellido LIKE '%" + palabra + "%')";
+                    }
+                    //datosEncontrados = "(codigo LIKE '%" + palabra + "%' OR nombre LIKE '%" + palabra + "%' OR apellido LIKE '%" + palabra + "%')";
+                }
+                else
+                {
+                    if (int.TryParse(palabra, out int codigoNumero))
+                    {
+                        datosEncontrados += " AND (codigo = " + codigoNumero + " OR nombre LIKE '%" + palabra + "%' OR apellido LIKE '%" + palabra + "%')";
+                    }
+                    else
+                    {
+                        datosEncontrados += " AND (nombre LIKE '%" + palabra + "%' OR apellido LIKE '%" + palabra + "%')";
+                    }
+                    //datosEncontrados += " AND(codigo LIKE '%" + palabra + "%' OR nombre LIKE '%" + palabra + "%' OR apellido LIKE '%" + palabra + "%')";
+                }
+            }
+            this.contenido.RowFilter = datosEncontrados;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.metodoFiltrar("SELECT * FROM Agenda", ref datosTemporales, "Agenda");
+            this.contenido = ((DataTable)datosTemporales.Tables["Agenda"]).DefaultView;
+            this.dataGridView1.DataSource = contenido;
+        }
+    }
+}
